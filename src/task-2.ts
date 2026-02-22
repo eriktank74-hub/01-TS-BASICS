7bd8ca3d091fe04cd7623b58bd4e34df8bb97e96
Erik <erik@Eriks-MacBook-Pro.local>
2026-02-22 17:35:43 +0200

    01-TS-BASICS

---
+++ src/task-2.ts
@@ -0,0 +1,11 @@
+const product: {
+    id: number;
+    title: string;
+    description: string
+} = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+}
+
+console.log(`Product: ${JSON.stringify(product)}`);
