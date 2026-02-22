1c46c9d2e029a365cb83de1029dc9f9b4149aaea
eriktank74-hub <eriktank74@gmail.com>
2026-02-22 15:41:10 +0000

    Deploying to gh-pages from @ eriktank74-hub/01-TS-BASICS@a149f274cbae7b358597c75c34f0f5e2ca4ef1b8 🚀

--- src/task-6.ts
+++
@@ -1,9 +0,0 @@
-function getFirstElement<T>(arr: T[]) {
-  return arr[0];
-}
-
-getFirstElement<number>([1, 2, 3]);           // 1
-getFirstElement<string>(["a", "b", "c"]);     // "a"
-getFirstElement<boolean>([true, false, true]); // true
-
-
